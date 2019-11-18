a6bd796cef1602696257031b2a8606ba6a3e383e
kajiwara111 <y.kaji.pharm1999@gmail.com>
2019-11-18 18:27:04 +0900

    モデルバリデーションとそのテスト追加、バリデーションエラー時viewに表示されるよう修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_165320) do
+ActiveRecord::Schema.define(version: 2019_11_18_035230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "task_name"
+    t.string "task_name", limit: 30, null: false
     t.text "task_content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'task_nameが存在しかつ30文字以内、およびtask_contentが300文字以内であれば有効なこと' do
+    expect(FactoryBot.build(:task)).to be_valid
+  end
+
+  it "task_nameが空ならバリデーションが通らない" do
+    task = FactoryBot.build(:task, task_name: '', task_content: '失敗テスト')
+    expect(task).not_to be_valid
+  end
+
+  it 'task_nameが31文字以上ならバリデーションが通らない' do
+    test_str = "a" * 31
+    task = FactoryBot.build(:task, task_name: test_str, task_content: '失敗テスト')
+    expect(task).not_to be_valid
+  end 
+
+  it 'task_contentが301文字以上ならバリデーションが通らない' do
+    test_str = "a" * 301
+    task = FactoryBot.build(:task, task_name: 'test', task_content: test_str)
+    expect(task).not_to be_valid
+  end
 end
+
